8709d2ebfc0be68ac0541d4d4ef88b1768f06042
banana <banana@eurus.cn>
2019-03-21 22:05:40 +0800

    update examples to v1.1.1

--- examples/public/dist/dev/index.js
+++ examples/public/dist/dev/index.js
@@ -19252,6 +19252,7 @@ function () {
   }, {
     key: "query",
     value: function query(vnode) {
+      this.page = 0;
       var options = {
         keys: this.headers.map(function (_ref) {
           var key = _ref.key,
