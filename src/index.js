5af18f0280ba164005f53667825b7718a9b99551
banana <banana@eurus.cn>
2019-03-21 15:56:18 +0800

    change export style

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import simpleTable from './components/tables/simple_table.js'
 import simpleSearchTable from './components/tables/simple_search_table.js'
 import remoteSearchTable from './components/tables/remote_search_table.js'
 
-export {
-  simpleTable,
-  simpleSearchTable,
-  remoteSearchTable
-}
+const MithrilWidgets = { simpleTable, simpleSearchTable, remoteSearchTable }
+export default MithrilWidgets
